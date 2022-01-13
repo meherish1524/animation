ba20248275d58baf63acef8fff258a7129c5790f
manchala meherish <manchala.meherish1524@gmail.com>
2022-01-13 14:03:00 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,35 @@
+import time
+
+import cv2
+
+import numpy as np
+
+img = cv2.imread("IMAGE_NAME")
+
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+gray = cv2.medianBlur(gray, 1)
+
+edges1= cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 9, 9)
+
+# Cartoonization
+
+color = cv2.bilateralFilter(img, 9, 250, 250)
+
+cartoon1 = cv2.bitwise_and(color, color, mask=edges1)
+
+cv2.imshow("Image", img)
+
+cv2.imshow("edges1", edges1)
+
+cv2.waitKey(0)
+
+cv2.destroyAllWindows()
+
+cv2.imshow("Image", img)
+
+cv2.imshow("Cartoon1", cartoon1)
+
+cv2.waitKey(0)
+
+cv2.destroyAllWindows()
